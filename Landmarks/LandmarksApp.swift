0d5baca9700526c9626d6c029023ca287c7249bd
Nikola Anastasovski <nikola.anastasovski@netcetera.com>
2021-02-21 00:43:24 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nikola Anastasovski on 21.2.21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
